8b5f6f40caf03598c7c8bd27ad744f979458f64b
jefary <0755607bv@gmail.com>
2022-09-20 18:28:52 -0700

    Added some functions and worked on main

--- main.cc
+++ main.cc
@@ -1,6 +1,22 @@
 //Fill out this comment with your names and which bullet points you did
-//Partners:
+//Partners: Brian Vega
 //Bullet Points: 
 //Extra Credit: 
 //URL to cover art and music: 
 
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+void print_map(const vector<vector<char>>& vec) {
+	return; //stub me out
+}
+
+void set_map(const vector<vector<char>>& vec) {
+	return; //stubbed
+}
+
+int main() {
+	vector<vector<char>> map; //holds map
+}
